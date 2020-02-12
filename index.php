da2bfbab4c24384f8d676b1997f905c74ea79296
shaakilahammed <60335472+shaakilahammed@users.noreply.github.com>
2020-02-12 21:39:56 +0600

    task-3(Modified)

--- index.php
+++ index.php
@@ -6,10 +6,8 @@
 </head>
 <body>
 	<?php
-		$name=$email=$date=$exam=$gender=$bloodGroup=$month=$year="";
+		$name=$email=$date=$exam=$gender=$bloodGroup="";
 		$nameErr = $emailErr = $dateErr=$examErr= $genderErr=$bgErr= $monthErr = $yearErr = "";
-	?>
-	<?php
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			if (empty($_POST["name"])){
 				$nameErr = "Name is required";
@@ -23,100 +21,127 @@
 			else {
 				$email = $_POST["email"];
 			}
-			if (empty($_POST["dd"])){
+			
+			if (empty($_POST["date"])){
 				$dateErr = "Date is required";
 			} 
 			else {
-				$date = $_POST["dd"];
+				$date = $_POST["date"];
 			}
-			if (empty($_POST["mm"])){
-				$monthErr = "Month is required";
-			} 
-			else {
-				$month = $_POST["mm"];
-			}
-			if (empty($_POST["yyyy"])){
-				$yearErr = "Year is required";
-			} 
-			else {
-				$year = $_POST["yyyy"];
-			}
+			
+			
 			if (empty($_POST["gender"])){
 				$genderErr = "Gender is required";
 			} 
 			else {
 				$gender = $_POST["gender"];
 			}
-			if (empty($_POST["bloodGroup"])){
+			if (($_POST["bloodGroup"])=="none"){
 				$bgErr = "BloodGroup is required";
 			} 
 			else {
 				$bloodGroup = $_POST["bloodGroup"];
 			}
 			
+			if (empty($_POST["ssc"]) || empty($_POST["hsc"])){
+				$examErr = "SSC and HSC are requird";
+			} 
+			else {
+				
+				$exam = $_POST["ssc"].",".$_POST["hsc"];
+				
+				if(!empty($_POST["bsc"]))
+					$exam =$exam.", ".$_POST["bsc"];
+				if(!empty($_POST["msc"]))
+					$exam =$exam.", ".$_POST["msc"];
+			}
+			
 			
 		}
 	?>
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-		<b>Name :</b>
-		<input type="text" name="name"/>
-		<span style="color:red;">* <?php echo $nameErr;?></span>
-		<br /><br />
+		<table>
+			<tr>
+				<td><b>Name :</b></td>
+				<td><input type="text" name="name"/></td>
+				<td><span style="color:red;">* <?php echo $nameErr;?></span></td>
+			</tr>
+			<tr>
+				<td><b>Email :</b></td>
+				<td><input type="email" name="email"/></td>
+				<td><span style="color:red;">* <?php echo $emailErr;?></span></td>
+			</tr>
+			<tr>
+				<td><b>Date of Birth :</b></td>
+				<td><input type="Date" name="date"/></td>
+				<td><span style="color:red;">* <?php echo $dateErr;?></span></td>
+			</tr>
+			<tr>
+				<td><b>Gender :</b></td>
+				<td>
+					<input type="radio" value="Male" name="gender"/>Male
+					<input type="radio" value="Female" name="gender"/>Female
+					<input type="radio" value="Other" name="gender"/>Others</td>
+				<td><span style="color:red;">* <?php echo $genderErr;?></span></td>
+			</tr>
+			<tr>
+				<td><b>Degree :</b></td>
+				<td>
+					<input type="checkbox" name="ssc" value="SSC"/>SSC
+					<input type="checkbox" name="hsc" value="HSC"/>HSC
+					<input type="checkbox" name="bsc" value="BSC"/>BSC
+					<input type="checkbox" name="msc" value="MSC"/>MSC
+				</td>
+				<td><span style="color:red;">* <?php echo $examErr;?></span></td>
+			</tr>
+			<tr>
+				<td><b>Blood Group :</b></td>
+				<td>
+					<select name="bloodGroup">
+						<option value="none"></option>
+						<option value="A+">A+</option>
+						<option value="A-">A-</option>
+						<option value="B+">B+</option>
+						<option value="B-">B-</option>
+						<option value="AB+">AB+</option>
+						<option value="AB-">AB-</option>
+						<option value="O+">O+</option>
+						<option value="O-">O-</option>
+					</select>
+				</td>
+				<td><span style="color:red;">* <?php echo $bgErr;?></span></td>
+			</tr>
+			<tr>
+				<td align="center" colspan="2"><input type="submit" value="Submit" /></td>
+				
+			</tr>
+			
+		</table>
 		
-		<b>Email :</b>
-		<input type="email" name="email"/>
-		<span style="color:red;">* <?php echo $emailErr;?></span>
-		<br /><br />
 		
-		<b>Date of Birth :</b>
-		<input type="number" name="dd"/>
-		<span style="color:red;">* <?php echo $dateErr;?></span>
-		<b>/</b>
-		<input type="number" name="mm"/>
-		<span style="color:red;">* <?php echo $monthErr;?></span>
-		<b>/</b>
-		<input type="number" name="yyyy"/>
-		<span style="color:red;">* <?php echo $yearErr;?></span>
 		
-		<br /><br />
-		<b>Gender :</b>
-		<input type="radio" value="Male" name="gender"/>
-		Male
-		<input type="radio" value="Female" name="gender"/>
-		Female
-		<input type="radio" value="Other" name="gender"/>
-		Other<span style="color:red;">* <?php echo $genderErr;?></span>
-		<br /><br />
 		
-		<input type="checkbox" name="ssc" value="SSC"/>SSC
-		<input type="checkbox" name="hsc" value="HSC"/>HSC
-		<input type="checkbox" name="bsc" value="BSC"/>BSC
-		<input type="checkbox" name="msc" value="MSC"/>MSC
-		<span style="color:red;">* <?php echo $examErr;?></span>
-		<br /><br />
-		<b>Blood Group :</b>
-		<select name="bloodGroup">
-			<option value="A+">A+</option>
-			<option value="A-">A-</option>
-			<option value="B+">B+</option>
-			<option value="B-">B-</option>
-			<option value="AB+">AB+</option>
-			<option value="AB-">AB-</option>
-			<option value="O+">O+</option>
-			<option value="O-">O-</option>
-		</select>
-		<span style="color:red;">* <?php echo $bgErr;?></span>
-		<br /><br />
 		
-		<input type="submit" value="Submit"></input>
+		
+		
+		
+		
+		
+		
+		
 	</form>
 	<br><br>
-	<h3>Name :<?php echo $name;?></h3>
-	<h3>Email :<?php echo $email;?></h3>
-	<h3>Date of Birth :<?php echo $date."/".$month."/".$year;?></h3>
-	<h3>Gender :<?php echo $gender;?></h3>
-	
-	<h3>Blood group :<?php echo $bloodGroup;?></h3>
+	<?php
+	if($name!="" && $email!="" && $date!="" && $exam!="" && $gender!="" && $bloodGroup!="")
+		{
+			echo "<h3>Name : $name</h3>";
+			echo "<h3>Email : $email</h3>";
+			echo "<h3>Date of Birth : $date</h3>";
+			echo "<h3>Gender : $gender</h3>";
+			echo "<h3>Degree : $exam</h3>";
+			echo "<h3>Blood group : $bloodGroup</h3>";
+		}
+	?>
 	
 </body>
 </html>
